5671698788356e7571fb7cbb237a7f21ad534201
delphifreak <cpp.create@gmail.com>
2014-03-07 15:14:02 +0400

    Added settings

--- counter.v
+++ counter.v
@@ -6,16 +6,24 @@ module count #(
     input   wire                      clk,
     input   wire                      reset_n,
     input   wire                      enable,
+    input   wire [7:0]                counterMode,
     input   wire [COUNTER_SIZE-1 : 0] match_value,
     
     output  wire [COUNTER_SIZE-1 : 0] count,
     output  wire                      match_occured,
     output  wire                      overflow
 );
+wire dir;
+assign dir = counterMode[0];
+wire [1:0] ovfMode;
+assign ovfMode = counterMode[2:1];
+//00 - считаем до MAX
+//01 - считаем до match_value
+
 reg [COUNTER_SIZE-1 : 0] val;
-always @ ( posedge clk ) begin
+always @ ( posedge clk or negedge reset_n) begin
     if (!reset_n) begin
-        val<=0;
+        val <= 0;
     end else if (enable) begin
         val <= val + 1;
     end
@@ -30,5 +38,3 @@ assign match_occured = (val == match_value);
 
 
 endmodule
-
-
